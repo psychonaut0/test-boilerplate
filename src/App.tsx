e5d9cff904d8d16d8b32ce96abf38349132d0bbd
Francesco Barbano <francesco.barbano@mexage.net>
2024-07-23 17:18:15 +0200

    format

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div className="flex h-full w-full items-start justify-center bg-black text-4xl underline hover:bg-white">
-                Hello!
+      Hello!
     </div>
   );
 }
